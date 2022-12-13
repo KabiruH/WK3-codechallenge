6fe3ea84eaab931f206726e9735a723240381df1
KabiruH <kelkab46@gmail.com>
2022-12-14 01:34:11 +0300

    few changes

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-// //URLS
-// const allMovies = "http://localhost:3000/films"
-// //Movie Lists
 
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     function moviesList () {
-//         fetch (allMovies)
-//         .then((response) => response.json())
-//         .then((data) => {
-
-//                 let movies = document.getElementsByTagName ("ol") [0]
-//                 let movieId = document.getElementById("id") 
-//                 let movieTitle = document.getElementById ("title")
-//                 let movieRuntime = document.getElementById ("runtime")
-//                 let movieShowtime = document.getElementById ("showtime")
-//                 let movieCapacity = document.getElementById ("capacity")
-//                 let movieTickets = document.getElementsById ("tickets-sold")
-//                 let movieDescription = document.getElementsByTagName ("p")
-//                 let moviePoster = document.getElementsByTagName ("img")
-            
-//         })
-
-        
-        
-
-//     }    
-//  })
 
 fetch("http://localhost:3000/films")
 .then (resp => resp.json())
@@ -34,33 +6,38 @@ fetch("http://localhost:3000/films")
    // console.log(data)
 
   const firstFilm = data.find((object) => object.id == 1)
-
+//Show poster of the first movie and its details
 
     let details = document.getElementsByClassName("Details")
-    title.innerText = firstFilm.title
+    title.innerText =  firstFilm.title
     runtime.innerText = firstFilm.runtime
     showtime.innerText = firstFilm.showtime
-
+    capacity.innerText = firstFilm.capacity
+    tickets_sold.innerText = firstFilm.tickets_sold
     description.innerText = firstFilm.description
+
+    
 
     const poster = document.getElementById("poster")
     const image = document.createElement("img")
     image.src = firstFilm.poster
     image.alt = "poster image"
-    image.width = "400"
+    image.width = "300"
     image.height = "400"
     poster.appendChild(image)
 
+    //purchasing tickets
     const remainingTickets = firstFilm.capacity-firstFilm.tickets_sold
-    const availableTickets = document.createElement ("span")
+    const availableTickets = document.createElement ("button")
     availableTickets.innerText = `${remainingTickets} remaining tickets`
     details.appendChild (availableTickets)
 
     const ticketButton = document.getElementById ("buy")
     ticketButton.addEventListener("click", () =>{
       if (availableTickets < 1){
-        availableTickets.innerText = "SOLD OUT"
-        alert("no more tickets")
+        ticketButton.innerText = "SOLD OUT"
+        availableTickets.innerText = ""
+        
         }
         else {
           --availableTickets}
